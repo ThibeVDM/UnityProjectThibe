9bd085136f56a6c374294b3c4b8ed34393e9aef2
ThibeVDM <thibe.vandemoortele@ll.sintgregorius.be>
2025-10-09 22:40:16 +0200

    movement added but jump error

---
+++ Assets/Scenes/MovementScript.cs
@@ -0,0 +1,49 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            rb.linearVelocity = Vector2.up * jumpingPower;
+        }
+        
+
+        Flip();
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocityY);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
